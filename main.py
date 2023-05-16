b7dd27c3e348fd9db209bb1b1dca82541bb9c521
jumc571 <justin.mcmichen@colorado.edu>
2023-05-15 21:51:06 -0600

    Working copy

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 from logging.config import fileConfig
 from pathlib import Path
 from unicodedata import normalize
@@ -13,6 +17,7 @@ from selenium.webdriver.common.by import By
 from sqlalchemy import create_engine
 from sqlalchemy import URL
 from datetime import date
+from email import encoders
 # from pandasql import sqldf
 
 import pandas as pd
@@ -40,7 +45,15 @@ def main():
         "mssql+pyodbc", 
         query={"odbc_connect": conn_str}
     )
-    engine = create_engine(connection_url, use_setinputsizes=False)
+    staging_engine = create_engine(connection_url, use_setinputsizes=False)
+
+    conn_str = 'DRIVER={SQL Server};Server=DESKTOP-UB80N6S;Database=GG;Trusted_Connection=True;'
+    # cnxn = pyodbc.connect(conn_str)
+    connection_url = URL.create(
+        "mssql+pyodbc", 
+        query={"odbc_connect": conn_str}
+    )
+    gg_engine = create_engine(connection_url, use_setinputsizes=False)
     # engine = create_engine(conn_str)
 
     url = 'http://ratingupdate.info/player/2EC3DA77B57ADAA/RA'
@@ -85,59 +98,11 @@ def main():
     print(df)
 
     if df.empty == False:
-        df.to_sql('Games', con=engine, schema='dbo', if_exists='append', index=False)
+        df.to_sql('Games', con=staging_engine, schema='dbo', if_exists='append', index=False)
         print('Rows uploaded')
 
-################################PS############################################
-
-# url = 'http://ratingupdate.info/player/30F67F37E15B4E5/RA'
-# browser = webdriver.Chrome()
-# browser.implicitly_wait(10)
-# browser.get(url)
-# browser.find_element(By.XPATH, """//*[@id="history"]/div/table""")
-
-# html = browser.page_source
-
-# table_MN = pd.read_html(html)
-# print(len(table_MN))
-
-# df = table_MN[0]
-# df = df.drop('Floor', axis='columns')
-# df['wins'] = df['Result'].str[:2]
-# df['wins'].fillna(0)
-# df['wins'] = df['wins'].astype(int)
-# df['losses'] = df['Result'].str[-2:]
-# df['losses'].fillna(0)
-# df['losses'] = df['losses'].astype(int)
-
-# cols = {
-# 'Date':'data_datetime',
-# 	'Rating':'my_rating',
-# 	'Opponent':'opp_name',
-# 	'Character':'opp_char',
-# 	'Rating.1':'opp_rating',
-# 	'Odds':'win_odds', 
-#     'wins':'wins',
-#     'losses':'losses',
-#      'Rating change':'rating_change' }
-# df.rename(columns = cols, inplace = True)
-
-# today = date.today().strftime("%Y-%m-%d")
-# print("Today's date:", today)
-# q = f"SELECT * FROM df WHERE [data_datetime] like '{today}%'"
-# # q = f"SELECT * FROM df WHERE [data_datetime] like '2023-03-19%'"
-# df = sqldf.run(q)
-# if df.empty == False:
-#     df = df.drop('index', axis='columns')
-#     print(df)
-
-
-#     df.to_sql('Games', con=engine, schema='dbo', if_exists='append', index=False)
-#     print('Rows uploaded')
-
-
-#EXAMPLE EMAIL PROC
-    engine.dispose()
+    staging_engine.dispose()
+    gg_engine.dispose()
     log.debug('Exiting main()')
 
 if __name__ == '__main__':
